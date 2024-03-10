4d47d4f82af7569cba28501324418b52a0a0e5fa
enuchsa <enuchsa@gmail.com>
2024-03-09 21:32:25 -0300

    refactor: ProductController and ProductRequestDTO
    now have a service and the methods thats use repository in controller are changes by services methods

--- src/main/java/com/gatitus/devcaixa/controllers/ProductController.java
+++ src/main/java/com/gatitus/devcaixa/controllers/ProductController.java
@@ -3,55 +3,74 @@ package com.gatitus.devcaixa.controllers;
 import com.gatitus.devcaixa.domain.product.Product;
 import com.gatitus.devcaixa.domain.product.ProductRequestDTO;
 import com.gatitus.devcaixa.domain.product.ProductResponseDTO;
-import com.gatitus.devcaixa.repositories.ProductRepository;
+import com.gatitus.devcaixa.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("product")
 public class ProductController {
 
+    private ProductService productService;
+
     @Autowired
-    private ProductRepository repository;
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping
     public List<ProductResponseDTO> getAll() {
-        return repository.findAll().stream().map(ProductResponseDTO::new).toList();
+        return this.productService.list().stream().map(ProductResponseDTO::new).toList();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponseDTO> getOne(@PathVariable String id) {
-        Product product = repository.findById(id).orElse(null);
+        Optional<Product> product = productService.find(id);
 
-        if (product == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok().body(new ProductResponseDTO(product));
+        return product.map(value -> ResponseEntity.ok().body(new ProductResponseDTO(value))).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<ProductResponseDTO> save(@RequestBody ProductRequestDTO productRequestDTO) {
         Product product = new Product(productRequestDTO);
-        repository.save(product);
+        this.productService.insert(product);
 
         return ResponseEntity.status(201).build();
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ProductResponseDTO> update(@PathVariable String id, @RequestBody ProductRequestDTO productRequestDTO) {
-        Product product = repository.findById(id).orElse(null);
+        Optional<Product> optionalProduct = this.productService.find(id);
 
-        if (product == null) {
+        if (optionalProduct.isEmpty()) {
             return ResponseEntity.status(400).build();
         }
 
-        product = new Product(productRequestDTO);
+        Product product = new Product(productRequestDTO);
         product.setId(id);
-        repository.saveAndFlush(product);
+        this.productService.update(product);
+
+        return ResponseEntity.status(201).build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> updateStock(@PathVariable String id, @RequestBody ProductRequestDTO productRequestDTO) {
+        Optional<Product> optionalProduct = this.productService.find(id);
+
+        if (optionalProduct.isEmpty()) {
+            return ResponseEntity.status(400).build();
+        }
+
+        Product product = optionalProduct.get();
+        product.setId(id);
+        product.setPrice(productRequestDTO.price());
+        product.setBuyPrice(productRequestDTO.buyPrice());
+        product.setAmount(productRequestDTO.amount());
+        this.productService.update(product);
 
         return ResponseEntity.status(201).build();
     }
--- src/main/java/com/gatitus/devcaixa/domain/product/ProductRequestDTO.java
+++ src/main/java/com/gatitus/devcaixa/domain/product/ProductRequestDTO.java
@@ -2,6 +2,6 @@ package com.gatitus.devcaixa.domain.product;
 
 import com.gatitus.devcaixa.domain.type.Type;
 
-public record ProductRequestDTO(String id, String name, String code, String brand,
+public record ProductRequestDTO(String name, String code, String brand,
                                 Float price, Float buyPrice, Integer amount, Type type) {
 }
